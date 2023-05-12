a0eda3aa6db4b164826aebb0a0a7b6cd43b06495
linaXta <alina.rubcova0418@gmail.com>
2023-05-12 10:06:59 +0200

    IProductRepo: Interfeisa izveidošana

---
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -0,0 +1,28 @@
+package lv.venta.repo;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.models.Product;
+
+public interface IProductRepo extends CrudRepository<Product, Integer>{
+	
+	//select * from Product_table where price < variable;
+	ArrayList<Product> findByPriceLessThan(float var);
+	
+	//atlasa pēc nosaukuma (Var būt vairāki vienādi nosaukumi)
+	//select * from product_table where title ="var"
+	ArrayList<Product> findByTitleIgnoreCase(String var);
+	
+	//atlasa tos produktus, kuru daudzums ir lielāks par desmit bet cena mazāk par 4 euro
+	//select * from produt_table where quantity> varQ And price < varP
+	ArrayList<Product> findByQuantityGreaterThanAndPriceLessThan(int vawQ, float varP);
+	
+	//@Query(nativeQuery = countBy)
+	//ArrayList<Product> funcNameDoesNotMatter();
+	
+	
+
+}
