e055d674486f5efada973ead96a6782d8a10dd69
linaXta <alina.rubcova0418@gmail.com>
2023-05-12 09:34:55 +0200

    DB: jpa anotāciju pievienšana

--- src/main/java/lv/venta/models/Product.java
+++ src/main/java/lv/venta/models/Product.java
@@ -3,43 +3,54 @@ package lv.venta.models;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Table(name = "product_table") //DB pusē izveidosies tabula
+@Entity
 public class Product {
 
+	@Column(name = "Id")  //DB pusē būs kolona "id" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+	@Column(name = "Title") //DB pusē kolonna "Title"
 	@NotNull
 	@Pattern(regexp = "[A-ZĀĢČĒĪĶĻŅŠŪŽ]{1}[a-zāģčēīķļņšūž\\ ]+")
 	@Size(min = 3, max = 30, message = "Jābūt vismaz 3 simbola garam un nepārsniegt 30")
 	private String title;
 	
+	@Column(name = "Price")//DB pusē kolonna "Price"
 	@Min(value = 0)
 	@Max(value = 1000)
 	private float price;
 	
+	@Column(name = "Description")//DB pusē kolonna "Description"
 	@NotNull
 	@Pattern(regexp = "[A-ZĀČĒĪĶĻŅŠŪŽ]{1}[a-zāčēīķļņšūž\\ ]+")
 	@Size(min = 3, max = 100)
 	private String description;
 	
+	@Column(name = "Quantity")//DB pusē kolonna "Quantity"
 	@Min(value = 1)
 	@Max(value = 1000)
 	private int quantity;
 	
-	private static int idCounter = 0;
 	
 	public int getId() {
 		return id;
 	}
 
-	public void setId() {
-		this.id = idCounter++;
-	}
 
 	public String getTitle() {
 		return title;
@@ -74,7 +85,6 @@ public class Product {
 	}
 
 	public Product(String title, float price, String description, int quantity) {
-		setId();
 		this.title = title;
 		this.price = price;
 		this.description = description;
